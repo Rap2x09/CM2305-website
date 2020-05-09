4d1c9e2ebee13fafa1dd16deaa8dd00afac1970b
Raphael Ramos <ramosri@cardiff.ac.uk>
2020-05-09 01:32:11 +0100

    .

--- group_project/settings.py
+++ group_project/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
